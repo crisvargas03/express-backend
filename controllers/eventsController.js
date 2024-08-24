c1538b4886ff31185d7abf7cfa67430fda94bdb3
Cristian I. Vargas <civargas34@gmail.com>
2024-08-24 18:55:47 -0400

    test

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -41,6 +41,7 @@ const createEvent = async (req, res = response) => {
 };
 
 const updateEvent = async (req = request, res = response) => {
+	console.log(request);
 	const eventId = req.params.id;
 	try {
 		const findedEvent = await Event.findById(eventId);
